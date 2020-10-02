28433200ca55af581b8de572be5f5d847739855a
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-02 17:29:41 +0530

    [arijit]Added : Welcome Message

---
+++ src/main/java/com/cg/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.cg.addressbook;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Addressbook");	
+	} 
+}
---
+++ src/test/java/com/cg/addressbook/AddressBookTest.java
@@ -0,0 +1,12 @@
+
+package com.cg.addressbook;
+
+import org.junit.Test;
+
+import com.cg.addressbook.AddressBookMain;
+
+import static org.junit.Assert.*;
+
+public class AddressBookTest {
+    
+}
