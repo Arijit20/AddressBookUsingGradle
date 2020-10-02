16f3604e1203c03e05d0b164c0a4457544afddb4
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-02 23:34:24 +0530

    [Arijit]Updated : Duplicate Entry Not Allowed

--- src/main/java/com/cg/addressbook/AddressBookMain.java
+++ src/main/java/com/cg/addressbook/AddressBookMain.java
@@ -1,24 +1,25 @@
 package com.cg.addressbook;
+
 import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-		
+
 		AddressBookList addressBookList = new AddressBookList();
 		Scanner in = new Scanner(System.in);
 		boolean b = false;
 		System.out.println("Welcome to Addressbook");
-      
+
 		System.out.println("Enter 1 to open existing addressbook");
 		System.out.println("Enter 2 to open new addressbook");
 		System.out.println("Enter 3 to exit");
 		int option = in.nextInt();
-		while(option != 3) {
-			if(option == 1)
+		while (option != 3) {
+			if (option == 1)
 				addressBookList.openAddressBook();
-			else if(option == 2)
+			else if (option == 2)
 				addressBookList.newAddressBook();
-			else if(option == 3)
+			else if (option == 3)
 				break;
 			else
 				System.out.println("Enter Correct option");
@@ -26,5 +27,5 @@ public class AddressBookMain {
 			option = in.nextInt();
 		}
 		System.out.println("Thank You");
-}
+	}
 }
