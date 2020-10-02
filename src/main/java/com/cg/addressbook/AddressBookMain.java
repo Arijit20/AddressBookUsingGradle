a011c8ab880d9af5b345c68113675ef492e46818
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-02 19:58:40 +0530

    [Arijit]Added:Contact Details Added in Address Book

--- src/main/java/com/cg/addressbook/AddressBookMain.java
+++ src/main/java/com/cg/addressbook/AddressBookMain.java
@@ -3,5 +3,7 @@ package com.cg.addressbook;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Addressbook");	
+		Contacts contact = new Contacts();
+		
 	} 
 }
