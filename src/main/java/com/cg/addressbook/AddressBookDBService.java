9b38eb234d4911b57e0afe65950b53fef082461a
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-31 01:58:38 +0530

    [Arijit]Added : Method To Retrive Contacts Added In A Given Range Of Dates

--- src/main/java/com/cg/addressbook/AddressBookDBService.java
+++ src/main/java/com/cg/addressbook/AddressBookDBService.java
@@ -1,6 +1,7 @@
 package com.cg.addressbook;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -77,6 +78,23 @@ public class AddressBookDBService {
 				.filter(contact -> contact.getFirstName().contentEquals(firstName))
 				.findFirst()
 				.orElse(null);
+	}
+
+	public int getContactsOnDateRange(LocalDate startDate, LocalDate endDate) throws AddressBookDBException{
+		// TODO Auto-generated method stub
+		String sql = String.format("SELECT id FROM contacts WHERE start BETWEEN '%s' AND '%s';",
+				Date.valueOf(startDate), Date.valueOf(endDate));
+		int noOfContacts = 0;
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+			while (resultSet.next()) {
+				noOfContacts++;
+			}
+		} catch (SQLException e) {
+			throw new AddressBookDBException(AddressBookDBException.ExceptionType.CONNECTION_ERROR, e.getMessage());
+		}
+		return noOfContacts;
 	}	
 }
 
